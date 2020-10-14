bdf6d86804cc424966656b20c5740e0810c12ddd
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-10-14 12:41:03 +0530

    Protecting DI from minification

--- Proj 3-DependencyInjection/app.js
+++ Proj 3-DependencyInjection/app.js
@@ -4,7 +4,7 @@
     angular.module('DIApp', [])
     .controller('DIController', DIController);
 
-
+    DIController.$inject = ['$scope', '$filter'];
     function DIController($scope, $filter){
         $scope.name = "Manish"
 
